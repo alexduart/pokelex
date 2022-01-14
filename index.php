1206a8597f358cf14cd6895e83be30ade5397e67
Alex Duarte <alexduart@gmail.com>
2022-01-14 15:07:48 -0300

    asd

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+require 'vendor/autoload.php';
+use GuzzleHttp\Client;
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -32,16 +36,60 @@
 		</div>
 	</header>
 
+	<?php
+	function debug($a){
+		echo '<pre>';
+		print_r($a);
+		echo '</pre>';
+	}
+
+	function listaPokemon($offset = 0, $limit = 10){
+		$url_base 		= "https://pokeapi.co/api/v2/";
+		$headers        = array("Content-Type" => "application/json");
+		$form_params 	= '{}';
+		$client = new Client([
+			'base_uri'    	=> $url_base,
+			'headers'       => $headers,
+			'timeout'       => 10.0
+		]);
+
+		$return = $client->request('GET', 'pokemon?offset='.$offset.'&limit='.$limit, ['debug' => false, 'body' => $form_params, 'http_errors' => true] );
+		$codRetorno       = $return->getStatusCode();
+		return $body 			  = json_decode($return->getBody());
+	}
+
+	function dadosPokemon($url){
+		$url_base 		= $url;
+		$headers        = array("Content-Type" => "application/json");
+		$form_params 	= '{}';
+		$client = new Client([
+			'base_uri'    	=> $url_base,
+			'headers'       => $headers,
+			'timeout'       => 10.0
+		]);
+
+		$return = $client->request('GET', '', ['debug' => false, 'body' => $form_params, 'http_errors' => true] );
+		$codRetorno       = $return->getStatusCode();
+		return $body 			  = json_decode($return->getBody());
+	}
+
+	$pokemons 		= listaPokemon(0, 640);
+	?>
+
 	<div class="container">
-		<?php for($a = 1; $a <= 150; $a++){ ?>
+		<?php foreach($pokemons->results as $pokemon){
+				$dados = dadosPokemon($pokemon->url);
+		?>
 			<div class="pokemons">
 				<div class="info">
-					<span class="numero">#<?php echo str_pad($a, 3, 0, STR_PAD_LEFT) ?></span>
-					<span class="nome">Pokemon</span>
-					<div class="tipo">Normal</div>
+					<span class="numero">#<?php echo str_pad($dados->game_indices[3]->game_index, 3, 0, STR_PAD_LEFT)  ?></span><br>
+					<span class="nome"><?php echo $pokemon->name; ?></span>
+					<?php foreach($dados->types as $tipo){ ?>
+					<div class="tipo"><?php echo $tipo->type->name ?></div>
+					<?php } ?>
 				</div>
 				<div class="imagem">
-					<div><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/<?php echo $a ?>.svg" alt=""></div>
+					<div><img src="<?php echo $dados->sprites->other->dream_world->front_default ?>" alt=""></div>
 				</div>
 			</div>
 		<?php } ?>
